5cf170087ec5737a6274ce12aa00effb1ed4f398
unknown <ahmedmiraz87@gmail.com>
2026-04-19 10:58:25 +0600

    Code bedugger app

---
+++ api_calling.py
@@ -0,0 +1,26 @@
+from google import genai
+from dotenv import load_dotenv
+import os,io
+import streamlit as st
+from gtts import gTTS
+
+
+load_dotenv()
+
+api_key= os.getenv("GEMINI_API_KEY")
+client=genai.Client(api_key=api_key)
+
+
+
+
+def generate_solution(images,selected_option):
+    
+    prompt=f"""Generate solution based on {selected_option},
+     if user need hint give him spme hint how he can resolve the bug in his system ,
+     or if user choose code give him the full code,
+    make sure to add markdown,"""
+    response=client.models.generate_content(
+    model="gemini-3-flash-preview",
+    contents=[images,prompt],
+   )
+    return response.text
---
+++ app.py
@@ -0,0 +1,65 @@
+import google as genai
+import os
+from dotenv import load_dotenv
+import streamlit as st
+from api_calling import generate_solution
+
+from PIL import Image
+
+
+load_dotenv()
+
+st.title("Code Debugger")
+st.markdown("Upload the screenshot")
+st.divider()
+
+
+
+
+
+with st.sidebar:
+        st.header("Controls")
+        
+        images = st.file_uploader(
+            "Upload a image",
+            type=["jpg","jpeg","png"],
+            accept_multiple_files=False
+            ) 
+        
+        
+        
+        
+        
+        
+            
+            
+            
+        if images:
+             converted_image=Image.open(images)
+     
+    
+    
+    # options
+    
+        selected_option=st.selectbox("Select the difficulties",("Hints","Code"),index=None)
+
+        submit_button=st.button("Submit",type="primary")
+
+if submit_button:
+    if not images:
+        st.error("Need to upload an image")
+    if not selected_option:
+        st.error("Please select an option")
+    if images and selected_option:
+        
+        with st.container(border=True):
+            st.subheader(f"Debugging ({selected_option})")
+            with st.spinner("Generating answer"):
+                answer = generate_solution(converted_image,selected_option)
+                st.markdown(answer)
+        
+            
+            
+            
+        
+
